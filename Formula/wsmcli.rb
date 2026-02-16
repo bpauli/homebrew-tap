61eaf8e772a809138f2da6fcedd6a1eec80c81c3
Burkhard Pauli <pauli@adobe.com>
2026-02-16 11:05:58 +0100

    Update wsmcli to v0.2.0

--- Formula/wsmcli.rb
+++ Formula/wsmcli.rb
@@ -1,9 +1,9 @@
 class Wsmcli < Formula
   desc "CLI for AEM Web Site Manager"
   homepage "https://github.com/bpauli/wsmcli"
-  version "0.1.0"
+  version "0.2.0"
   url "https://github.com/bpauli/wsmcli/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "ffa8e5a058365fdec25397caf8dbc4f67d183cd846473fd939d0edfac8e36db7"
+  sha256 "27860583b6cbf593ed638dce95095a0f3596b967b6ea03a5a508b92c5dab9582"
   license "Apache-2.0"
 
   depends_on "go" => :build
